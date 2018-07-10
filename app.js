83163a52b0363fbb5ed1156e32e1d046f823e8dc
klepra <klemen.pravdic@gmail.com>
2018-07-10 23:11:02 +0200

    simple logger test

--- app.js
+++ app.js
@@ -3,6 +3,20 @@ const path = require('path');
 const app = express();
 const parser = require('body-parser');
 const passport = require('passport');
+const bunyan = require('bunyan');
+const log = bunyan.createLogger({
+  name: 'myapp',
+  streams: [
+    {
+      level: 'info',
+      stream: process.stdout            // log INFO and above to stdout
+    },
+    {
+      level: 'info',
+      path: './app.log'  // log ERROR and above to a file
+    }
+  ]
+});
 
 app.use(express.static(path.join(__dirname, 'static')))
 app.use(parser.json());
@@ -19,5 +33,6 @@ app.all('/*', function (req, res) {
 });
 
 
+log.info("Application is set-up.");
 module.exports = app;
 

ead90d873e950ec2534509cc34829f96a6f9b046
klepra <klemen.pravdic@gmail.com>
2018-07-11 20:11:23 +0200

    prod env mongo

--- config/config.js
+++ config/config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  dbUrl: "mongodb://polls:polls@ds036617.mlab.com:36617/polls"
+  dbUrl: process.env.NODE_ENV === 'production' ? "mongodb://polls:polls@ds036617.mlab.com:36617/polls" : "mongodb://localhost:27017/polls"
 }
